3acff398eae7a8c6aed57507f8b3f86a2d5703d0
amnahasad <amnah_asad@hotmail.com>
2021-10-03 21:26:26 +0000

    Practice work for js fundamentals

---
+++ howManyHundreds.js
@@ -0,0 +1,12 @@
+function howManyHundreds(number) {
+
+  const hundredsCount = number / 100;
+
+  return Math.floor(hundredsCount);
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
